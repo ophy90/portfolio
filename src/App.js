d744c6dc5bf34ef990f395277289ed968ca7e422
“Ophélie <ophelie.bugnon@edu.hefr.ch>
2023-07-05 16:25:57 +0200

    modif in nodes_modules + creating NavBar component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
+import NavBar from "./components/NavBar";
 
+export default function App() {
 
-export default function PresenceList() {
-
-  const listStudents = studentInfos.map(studentInfos =>
-      <div
-        key={studentInfos.name}
-        style={{
-          color: studentInfos.isPresent ? 'green' : 'red'
-      }}>
-          {studentInfos.name} {studentInfos.surname} - {studentInfos.school}
-       </div>
-  )
 
   return (
       <div>
-          <h1>Presence list</h1>
-          <h1>{listStudents}</h1>
+          <NavBar/>
       </div>
   );
 }
 
-const studentInfos = [
-  {name: "Ophelie ", surname: "Bugnon", school: "heia", isPresent:true},
-  {name: "Toto ", surname: "Bugnon", school: "EPFL", isPresent:false},
-]
+
 
 
